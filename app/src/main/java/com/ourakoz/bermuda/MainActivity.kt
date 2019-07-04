f56b6ac7fdaeacca2eba9752a1a2bbae43ac2414
Antonin CHARRIER <charrier.antonin@yahoo.fr>
2019-07-04 09:54:04 +0200

    Favorites

--- app/src/main/java/com/ourakoz/bermuda/MainActivity.kt
+++ app/src/main/java/com/ourakoz/bermuda/MainActivity.kt
@@ -2,6 +2,9 @@ package com.ourakoz.bermuda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import java.time.LocalDateTime
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -22,8 +25,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var favBtn = findViewById<Button>(R.id.fav)
+        favBtn.setOnClickListener({ v -> artists.first { a -> a.name == "testArtist1" }.toggleFavorite() })
+    }
+
+    fun Artist.toggleFavorite() {
+        if (this.dateFavorite == null ) this.dateFavorite = LocalDateTime.now() else this.dateFavorite = null
     }
 }
 
-data class Artist(val name: String, val genres: ArrayList<Genre>, val dateFavorite: Date?)
+data class Artist(val name: String, val genres: ArrayList<Genre>, var dateFavorite: LocalDateTime?)
 data class Genre(val name: String)
